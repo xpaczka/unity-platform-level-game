bfc7a4bc2efe8cbad285275d460efcbe195b90d7
Michał Paczyński <michal.paka@op.pl>
2024-11-02 12:46:04 +0100

    Implement kill zone

--- Assets/Scripts/LevelFour.cs
+++ Assets/Scripts/LevelFour.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelFour : Gameplay {
-    public GameObject[] allChests;
     public int winningChestIndex;
 
     void Awake () {
-        allChests = GameObject.FindGameObjectsWithTag("all-chests");
+        GameObject[] allChests = GameObject.FindGameObjectsWithTag("all-chests");
         winningChestIndex = Random.Range(0, allChests.Length);
     }
 
